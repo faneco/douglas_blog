244a6c6a830c51f08a0bfe5f0402475340b755f0
faneco <ra13456.15@uninga.edu.br>
2016-10-09 15:31:08 -0300

    atulizacao05 nao pronta

--- main.py
+++ main.py
@@ -29,6 +29,32 @@ class Handler(webapp2.RequestHandler):
   def render(self, template, **kw):
     self.write(self.render_str(template, **kw))
 
+    # Handlers
+
+class MainHandler(Handler):
+  def get(self):
+    user_id = self.request.cookies.get("user_id")
+    if user_id:
+      self.render("main.html", logado = True)
+    else:
+      self.render("main.html", logado = False)
+
+class LoginHandler(Handler):
+  def get(self):
+self.render("login.html")
+
+def post(self):
+    username = self.request.get("username")
+    password = self.request.get("password")
+    user = User.query(User.username == username).get()
+if user and user.password == password:
+
+    #vai entrar aqui se o usuario exisitr
+ self.response.headers.add_header('Set-Cookie', 'user_id=%s; Path=/' % str(username))
+self.redirect("/")
+  else:
+    #vai entra aqui se o usuario nao existir
+    self.render("login.html", error = True)
 
 class MainHandler(Handler):
   def get(self):
