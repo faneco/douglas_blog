b8b9c4223ff4a42088da27382d29458f6c827cd8
faneco <ra13456.15@uninga.edu.br>
2016-10-17 13:32:10 -0200

    cookie

--- main.py
+++ main.py
@@ -1,12 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import webapp2
 import os
 import jinja2
+import hmac
+import random
+import string
+import hashlib
+
 
 from google.appengine.ext import ndb
 
+# Hashing de senha
+def make_salt():
+return ''.join(random.choice(string.letters) for x in xrange(5))
+
+def make_pw_hash(name, pw, salt = None):
+    if not salt:
+      salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+return '%s,%s' % (h, salt)
+
+def valid_pw(name, pw, h):
+    salt = h.split(',')[1]
+    if make_pw_hash(name, pw, salt) == h:
+        return True
+return False
 
 # Jinja2 Directory Configuration
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+# Criptografia de cookie
+SECRET = "Meu segredo..."
+
+def hash_str(s):
+    return hmac.new(SECRET, s).hexdigest()
+
+def make_secure_val(s):
+    return "%s|%s" % (s, hash_str(s))
+
+def check_secure_val(h):
+    val = h.split('|')[0]
+    if h == make_secure_val(val):
+return val
+
+
+# Jinja2 Directory Configuration
+
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
